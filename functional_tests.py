537b9bc886e7bc86d5a59efddf3eef3df65ec30f
Mohd Ali <mohd.ali@wealthpack.com>
2015-05-08 15:15:19 +0530

    Added unittest and made it class based

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'To-Do' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
 
-browser.quit()
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_list_and_retrieve_later(self):
+		self.browser.get('http://localhost:8000')
+
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Fininsh the test')
+
+if __name__ == '__main__':
+	unittest.main()	
