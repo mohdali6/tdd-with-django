0c549f8b7d9950b5969aee834c6584edcccc93c7
mohdali6 <mohd.ali6@yahoo.com>
2015-05-12 11:51:18 +0530

    FT checks for entered to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -14,7 +14,21 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Fininsh the test')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		slef.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+		inputbox.send_keys('Buy Apple Watch')
+		
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_tag_name('tr')
+		self.assertTrue(any(row.text == '1: Buy Apple Watch') for row in rows)
+
+		self.fail('Fininsh the test!')
 
 		#Define further
 		 
