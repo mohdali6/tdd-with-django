2023e94e911e5cb3db7670de064557a46d2c9837
mohdali6 <mohd.ali6@yahoo.com>
2015-05-14 13:11:31 +0530

    change FT, unit test and views for considering POST response

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -24,10 +25,20 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys('Buy Apple Watch')
 		
 		inputbox.send_keys(Keys.ENTER)
-
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue( any(row.text == '1: Buy Apple Watch' for row in rows), "New to-do element not in table")
+		self.assertIn('1: Buy Apple Watch', [row.text for row in rows])
+
+		# New text box to enter new to-do item.
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use Apple Watch')
+		inputbox.send_keys(Keys.ENTER)
+
+		# The page updates, and now shows both to-do items
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy Apple Watch', [row.text for row in rows])
+		self.assertIn('2: Use Apple Watch', [row.text for row in rows])
 
 		self.fail('Fininsh the test!')
 
